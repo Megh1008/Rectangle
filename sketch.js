2b5e65ef5e9b43128fbab4a088c8b921a588ec48
Megh1008 <74679068+Megh1008@users.noreply.github.com>
2020-12-30 18:50:59 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var fixedRect;
+var movingRect;
+function setup() {
+  createCanvas(1200,800);
+  fixedRect=createSprite(600,400,50,80);
+  fixedRect.shapeColor="green";
+  fixedRect.debug=true;
+  movingRect=createSprite(400,200,80,30);
+  movingRect.shapeColour="blue";
+  movingRect.debug=true;
+}
+
+function draw() {
+  background(0,0,0); 
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  drawSprites();
+}
